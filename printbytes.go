6bf9f017f21979daaba7bfd9ec0688f727d17311
Dmitry Chestnykh <dmitry@codingrobots.com>
2015-05-26 16:52:47 +0200

    Make it printbytes, to print anything from stdin.

--- printbytes.go
+++ printbytes.go
@@ -1,32 +1,29 @@
-// Command printrandombytes prints random bytes for use in Go programs.
+// Command printbytes reads bytes from standard input and prints them in Go
+// source format.
 package main
 
 import (
 	"bytes"
-	"crypto/rand"
-	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
-var fCount = flag.Int("c", 16, "number of bytes")
-
 func main() {
-	flag.Parse()
-	b := make([]byte, *fCount)
-	_, err := rand.Read(b[:])
+	in, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
 	buf := bytes.NewBuffer(nil)
 	lines := 1
-	for i, v := range b {
+	for i, v := range in {
 		if i > 0 && i%10 == 0 {
 			fmt.Fprintf(buf, "\n\t")
 			lines++
 		}
 		fmt.Fprintf(buf, "0x%02x", v)
-		if i != len(b)-1 {
+		if i != len(in)-1 {
 			fmt.Fprintf(buf, ", ")
 		}
 	}
